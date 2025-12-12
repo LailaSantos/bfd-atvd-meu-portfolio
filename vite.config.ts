4d77ed8d50d70cfc583bafe4ed72c18234f5e989
Laila Santos <lailaroberta100@gmail.com>
2025-12-11 21:34:03 -0300

    ajuste config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bfd-atvd-meu-portfolio/",
   plugins: [react()],
-  base: '/bfd-atvd-meu-portfolio/',
 })
