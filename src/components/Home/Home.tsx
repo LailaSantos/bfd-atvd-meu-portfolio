3548a84ea8092b546c583d8d1d984374f756abf1
Laila Santos <lailaroberta100@gmail.com>
2025-12-11 21:46:07 -0300

    ajuste nas rotas

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,10 @@
 import Card from "../Card/Card";
 import Footer from "../Footer/Footer";
 import "./Home.css";
+import fokus from "../../assets/fokus.png";
+import discord from "../../assets/discord.png";
+import nba from "../../assets/nba.png";
+import f1 from "../../assets/f1.png";
 
 function Home() {
   return (
@@ -10,7 +14,7 @@ function Home() {
           title="Projeto Fokus"
           description="Este projeto foi desenvolvido para fins de estudo, 
          com o objetivo de aprofundar o conhecimento em JavaScript, manipulando o DOM."
-          imageUrl="assets/fokus.png"
+          imageUrl={fokus}
           githubUrl="https://github.com/LailaSantos/Projeto-Fokus"
           siteUrl="https://projeto-fokus.netlify.app/"
         />
@@ -19,7 +23,7 @@ function Home() {
           title="Projeto Clone Discord"
           description="Este projeto foi desenvolvido para fins de estudo, seguindo um tutorial da Rocketseat. 
           É uma página estática inspirada no Discord e foi publicada no Netlify."
-          imageUrl="assets/discord.png"
+          imageUrl={discord}
           githubUrl="https://github.com/LailaSantos/yt-clone-discord"
           siteUrl="https://clone-discord-react.netlify.app/"
         />
@@ -28,7 +32,7 @@ function Home() {
           title="Projeto NBA"
           description="Este projeto foi desenvolvido para o curso Bolsa Futuro Digital da Softex Pernambuco, com o objetivo de praticar os conteúdos aprendidos nos primeiros módulos do curso, 
          utilizando HTML, CSS e JavaScript, além de aplicar conceitos de responsividade nas telas."
-          imageUrl="assets/nba.png"
+          imageUrl={nba}
           githubUrl="https://github.com/LailaSantos/programa-bolsa-futuro-digital-softex-recife"
           siteUrl="https://lailasantos.github.io/programa-bolsa-futuro-digital-softex-recife/index.html#inicio"
         />
@@ -37,7 +41,7 @@ function Home() {
           title="Projeto F1 Surveys"
           description="Este projeto foi desenvolvido para fins de estudo, com o objetivo de aprofundar o conhecimento em JavaScript, HTML e CSS no contexto de desenvolvimento web. Além disso, 
          o projeto permite que você conheça as equipes e pilotos da temporada 2023 da Fórmula 1."
-          imageUrl="assets/f1.png"
+          imageUrl={f1}
           githubUrl="https://github.com/LailaSantos/f1-surveys"
           siteUrl="https://f1-surveys.netlify.app/"
         />
